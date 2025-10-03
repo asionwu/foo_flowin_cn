4a88749f853ad4b8a25012efc238ff55ddcb62f6
Asion <asionwxk@gmail.com>
2025-10-03 12:28:16 +0800

    Update flowin_host.cpp

--- src/flowin_host.cpp
+++ src/flowin_host.cpp
@@ -859,7 +859,7 @@ private:
         SelectObjectScope scope(dc, (HGDIOBJ)callback_->query_font_ex(ui_font_default));
         CRect rc;
         GetClientRect(&rc);
-        dc.DrawText("点击添加新元件。", -1, &rc, DT_NOPREFIX | DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+        dc.DrawText(_T(pfc::stringcvt::string_os_from_utf8("点击添加新元件。")), -1, &rc, DT_NOPREFIX | DT_CENTER | DT_VCENTER | DT_SINGLELINE);
     }
 
     BOOL on_erase_bkgnd(CDCHandle dc)
